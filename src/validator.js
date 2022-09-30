abe7e00c812da23997c3b197516c77914a073eb6
notABluesSinger <rob@whatagoodbot.com>
2022-09-30 14:12:03 +0100

    Simplifying errors

--- src/validator.js
+++ src/validator.js
@@ -9,13 +9,9 @@ export class Validator {
   validate (data) {
     const isValid = this.schemaValidator(data)
     if (isValid) {
-      return {
-        error: false,
-        data
-      }
+      return data
     }
     return {
-      error: true,
       errors: this.schemaValidator.errors
     }
   }
