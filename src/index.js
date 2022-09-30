4214e47b92fa6babdd465abb292e60349d2fcab7
notABluesSinger <rob@whatagoodbot.com>
2022-09-30 11:39:52 +0100

    Might want to export that error validator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { connect } from 'mqtt'
 import mqttOptions from './config.js'
+
+import error from '../src/schemas/error/index.js'
 import responder from '../src/schemas/responder/index.js'
 
 const client = connect(mqttOptions.host, mqttOptions)
 
 export default {
   client,
+  error,
   responder
 }
