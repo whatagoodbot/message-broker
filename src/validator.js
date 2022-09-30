30dbf0b19e21a209e16775683f889b61655e614b
notABluesSinger <rob@whatagoodbot.com>
2022-09-30 15:02:05 +0100

    Allow union types

--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,5 @@
 import Ajv from 'ajv'
-const ajv = new Ajv({ allErrors: true })
+const ajv = new Ajv({ allErrors: true, allowUnionTypes: true })
 
 export class Validator {
   constructor (schema) {
