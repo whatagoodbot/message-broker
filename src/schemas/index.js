e3f9b82131b1941622fdfc8b800705ea9d50117f
notABluesSinger <rob@whatagoodbot.com>
2022-10-02 15:21:10 +0100

    Still fixing exports

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -6,21 +6,20 @@ import getAll from './responder/getAll.js'
 import systemError from './systemError.js'
 import chatMessage from './chatMessage.js'
 
-export default {
-  responder: {
-    add: {
-      request: new Validator(add.request),
-      response: new Validator(add.response)
-    },
-    get: {
-      request: new Validator(get.request),
-      response: new Validator(get.response)
-    },
-    getAll: {
-      request: new Validator(getAll.request),
-      response: new Validator(getAll.response)
-    }
+export const responder = {
+  add: {
+    request: new Validator(add.request),
+    response: new Validator(add.response)
   },
-  systemError: new Validator(systemError),
-  chatMessage: new Validator(chatMessage)
+  get: {
+    request: new Validator(get.request),
+    response: new Validator(get.response)
+  },
+  getAll: {
+    request: new Validator(getAll.request),
+    response: new Validator(getAll.response)
+  }
 }
+
+export const systemError = new Validator(systemError)
+export const chatMessage = new Validator(chatMessage)
