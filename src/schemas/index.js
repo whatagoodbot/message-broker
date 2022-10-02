f950d5b8704803d49d4fc060f1b13715ea2262f8
notABluesSinger <rob@whatagoodbot.com>
2022-10-02 15:24:21 +0100

    chat message export fix

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -4,7 +4,7 @@ import add from './responder/add.js'
 import get from './responder/get.js'
 import getAll from './responder/getAll.js'
 import * as sysError from './systemError.js'
-import chatMessage from './chatMessage.js'
+import * as chatMsg from './chatMessage.js'
 
 export const responder = {
   add: {
@@ -22,4 +22,4 @@ export const responder = {
 }
 
 export const systemError = new Validator(sysError)
-export const chatMessage = new Validator(chatMessage)
+export const chatMessage = new Validator(chatMsg)
