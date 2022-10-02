76327c71ff402aa54eaaf67762b48c2783d12cc8
notABluesSinger <rob@whatagoodbot.com>
2022-10-02 15:37:26 +0100

    removing namespacing

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -6,19 +6,17 @@ import getAll from './responder/getAll.js'
 import * as sysError from './systemError.js'
 import * as chatMsg from './chatMessage.js'
 
-export const responder = {
-  add: {
-    request: new Validator(add.request),
-    response: new Validator(add.response)
-  },
-  get: {
-    request: new Validator(get.request),
-    response: new Validator(get.response)
-  },
-  getAll: {
-    request: new Validator(getAll.request),
-    response: new Validator(getAll.response)
-  }
+export const addResponse = {
+  request: new Validator(add.request),
+  response: new Validator(add.response)
+}
+export const getResponse = {
+  request: new Validator(get.request),
+  response: new Validator(get.response)
+}
+export const getAllResponses = {
+  request: new Validator(getAll.request),
+  response: new Validator(getAll.response)
 }
 
 export const systemError = new Validator(sysError)
